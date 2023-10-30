1fc7c59e3cbdb1383d7a043ef2bf6f1003d95a28
Mori <mori@MoriM1.mshome.net>
2023-10-30 16:06:56 +0200

    IMDB dataset code

---
+++ IMDBdataset/IMDB.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.datasets import imdb
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data (num_words=10_000)
+
+word_index = imdb.get_word_index() # word_index is a dictionary mapping words to an integer index.
+reverse_word_index = dict(
+        [(value, key) for (key, value) in word_index.items()]) #Reverses it, mapping integer indices to words
+decoded_review = " ".join(
+    [reverse_word_index.get(i - 3, "?") for i in train_data[0]]) #Decodes the review. Note that the indices are offset by 3 because 0, 1, and 2 are reversed indices for "padding," "start of sequence," and "unknown"
+
+def vectorize_sequences(sequences, dimension=10_000):
+    results = np.zeros((len(sequences), dimension)) #Creates an all-zero matrix of shape (len(sequence),dimension)
+    for i, sequence in enumerate(sequences):
+        for j in sequence:
+            results[i, j] = 1. #sets specific indices of results[i] to 1s
+    return results
+x_train = vectorize_sequences(train_data) #Vectorized training data
+x_test = vectorize_sequences(test_data)   #Vectorized test data
+
+y_train = np.asarray(train_labels).astype("float32")
+y_test = np.asarray(test_labels).astype("float32")
+
+model = keras.Sequential([
+    layers.Dense(16, activation="relu"),
+    layers.Dense(16, activation="relu"),
+    layers.Dense(1, activation="sigmoid")
+])
+model.compile(optimizer="rmsprop",
+              loss="binary_crossentropy",
+              metrics=["accuracy"])
+
+x_val = x_train[:10_000]
+partial_x_train = x_train[10_000:]
+y_val = y_train[:10_000]
+partial_y_train = y_train[10_000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+history_dict = history.history
+loss_values = history_dict["loss"]
+val_loss_values = history_dict["val_loss"]
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, loss_values, "bo", label="Training loss")
+plt.plot(epochs, val_loss_values, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+plt.clf() #matplotlib figure clearing
+acc = history_dict["accuracy"]
+val_acc = history_dict["val_accuracy"]
+plt.plot(epochs, acc, "bo", label="Training acc")
+plt.plot(epochs, val_acc, "b", label="Validation acc")
+plt.title("Training and validation accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
+
+model = keras.Sequential([
+    layers.Dense(16, activation="relu"),
+    layers.Dense(16, activation="relu"),
+    layers.Dense(1, activation="sigmoid")
+])
+model.compile(optimizer="rmsprop",
+              loss="binary_crossentropy",
+              metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)
+
+""" The following experiments will help convince you that the architecture choices you’ve
+made are all fairly reasonable, although there’s still room for improvement:
+ You used two representation layers before the final classification layer. Try using
+one or three representation layers, and see how doing so affects validation and
+test accuracy.
+ Try using layers with more units or fewer units: 32 units, 64 units, and so on.
+ Try using the mse loss function instead of binary_crossentropy.
+ Try using the tanh activation (an activation that was popular in the early days of
+neural networks) instead of relu. """
