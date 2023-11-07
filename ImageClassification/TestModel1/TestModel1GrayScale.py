55694947c43281f0d7316ca495c8bdbdf4e51541
Mori <mori@78-27-67-120.bb.dnainternet.fi>
2023-11-07 17:13:58 +0200

    an attempt in grayscaling the image classification model

---
+++ ImageClassification/TestModel1/TestModel1GrayScale.py
@@ -0,0 +1,116 @@
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import keras_cv
+import os
+
+image_size = (180, 180)
+batch_size = 128
+
+train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="both",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+""" 
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+plt.show() #showing the images, should be put in another thread probably?
+ """
+#Adding random flips and rotation to the pictures to provide better learning 
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        keras_cv.layers.Grayscale(),
+    ]
+)
+
+val_data_augmentation = keras.Sequential(
+    [
+        keras_cv.layers.Grayscale(),
+    ]
+)
+
+""" plt.figure(figsize=(10, 10))
+for images, _ in train_ds.take(1):
+    for i in range(9):
+        augmented_images = data_augmentation(images)
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(augmented_images[0].numpy().astype("uint8"))
+        plt.axis("off")
+plt.show() """
+
+train_ds = train_ds.map(
+    lambda img, label: (data_augmentation(img), label),
+    num_parallel_calls=tf.data.AUTOTUNE, #The data will be fetched asynchronously while the training
+)
+
+val_ds = val_ds.map(
+    lambda img, label: (val_data_augmentation(img), label),
+    num_parallel_calls=tf.data.AUTOTUNE, #The data will be fetched asynchronously while the training
+)
+
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(inputs)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.Flatten()(x)
+    x = layers.Dropout(0.5)(x)
+    outputs =  layers.Dense(1, activation="sigmoid")(x)
+    return keras.Model(inputs, outputs)
+
+model = make_model(input_shape=image_size + (1,), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True) #Making the .png of a model
+
+epochs = 25
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("TestModel_at_{epoch}.keras"),
+]
+model.compile(
+    optimizer=keras.optimizers.legacy.Adam(1e-3),
+    loss = "binary_crossentropy",
+    metrics=["accuracy"],
+)
+history = model.fit(
+    train_ds,
+    epochs=epochs,
+    callbacks=callbacks,
+    validation_data=val_ds
+)
+
+history_dict = history.history
+loss_values = history_dict["loss"]
+val_loss_values = history_dict["val_loss"]
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, loss_values, "bo", label="Training loss")
+plt.plot(epochs, val_loss_values, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+plt.clf() #matplotlib figure clearing
+acc = history_dict["accuracy"]
+val_acc = history_dict["val_accuracy"]
+plt.plot(epochs, acc, "bo", label="Training acc")
+plt.plot(epochs, val_acc, "b", label="Validation acc")
+plt.title("Training and validation accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
